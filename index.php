93b2bba2154666b232f97fd1fb4fc2b202686b3e
InWahyu <in.wahyu@gmail.com>
2021-06-11 22:27:54 +0700

    Update index.php
    Diubah bagian body

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 <head><title>Hallo</title></head>
 <body><h1>Tes</h1>
 <p>Yes ini adalah halaman pertama yang dapat diupdate lewat Git</p>
+  <p>Alhamdulillah bisa dikit main git</p>
 </body>
 </html>
