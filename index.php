57a5380e5a1825712d0fc610d542b3b0a12b2805
InWahyu <in.wahyu@gmail.com>
2021-06-15 08:58:21 +0700

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head><title>Hallo</title></head>
-<body><h1>Tes</h1>
-<p>Yes ini adalah halaman pertama yang dapat diupdate lewat Git</p>
-  <p>Alhamdulillah bisa dikit main git</p>
-</body>
-</html>
